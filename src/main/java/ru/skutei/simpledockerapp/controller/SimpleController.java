225d52d593fd82e0c822f9c0d05a031d83596f8b
ksu-tei <2ksu.tei@gmail.com>
2024-01-19 18:14:45 +0300

    init commit

---
+++ src/main/java/ru/skutei/simpledockerapp/controller/SimpleController.java
@@ -0,0 +1,11 @@
+package ru.skutei.simpledockerapp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Docker!";
+    }
+}
